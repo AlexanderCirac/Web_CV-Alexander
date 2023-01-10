5659aec1dbdf546f860458205cd900f4fd51e5ac
Alexander Cirac Antio <95984829+AlexanderCirac@users.noreply.github.com>
2023-01-10 12:33:32 +0100

    Just refactor 3.0.4
    Implemented  Game controller for CVGame

---
+++ Assets/Scripts/SC_WebJuego/Game/GameController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UniRx;
+using UnityEngine.SceneManagement;
+
+namespace WebGame.Game
+{
+    public class GameController : MonoBehaviour
+    {
+        #region UnityCall
+        void Start()
+        {
+
+            Observable.EveryUpdate()
+                .Where(_ => Input.GetKeyDown(KeyCode.Alpha1))
+                .Subscribe(_ => { SceneManager.LoadScene(1); });
+        }
+        #endregion
+    }
+}
