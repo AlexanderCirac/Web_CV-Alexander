ff4c92d0c8e3ae6e4e2e8a1ea0b5b44ebc623ae3
Alexander Cirac Antio <95984829+AlexanderCirac@users.noreply.github.com>
2023-03-02 11:06:26 +0100

    Just refactor 3.0.8
    Just refactor 3.0.8

--- Assets/Scripts/SC_WebJuego/Templates/CharacterTemplate.cs
+++ Assets/Scripts/SC_WebJuego/Templates/CharacterTemplate.cs
@@ -11,17 +11,14 @@ namespace WebGame.Game.Templates
         #endregion
 
         #region UnityCalls
-        //private void Awake()
-        //{
-        //}
 
         private void Awake()
         {
             TryGetComponent(out _inputs);
             Observable.EveryUpdate().Where(_ => _inputs.GetJump() != 0).Subscribe(_ => _inputs._delegateInputsJump?.Invoke());
             _inputs._delegateInputsJump += ToJumping;
-            //Observable.EveryUpdate().Where(_ => _inputs.GetHorizontal() != 0 || GetVertical() != 0).Subscribe(_ => _delegateInputsMovement?.Invoke());
-            //Observable.EveryUpdate().Where(_ => GetRotationHorizontal() != 0 || GetRotationVertical() != 0).Subscribe(_ => _delegateInputsRotate?.Invoke());
+            Observable.EveryUpdate().Where(_ => _inputs.GetHorizontal() != 0 || _inputs.GetVertical() != 0).Subscribe(_ => _inputs._delegateInputsMovement?.Invoke());
+            Observable.EveryUpdate().Where(_ => _inputs.GetRotationHorizontal() != 0 || _inputs.GetRotationVertical() != 0).Subscribe(_ => _inputs._delegateInputsRotate?.Invoke());
         }
         private void OnDestroy()
         {
